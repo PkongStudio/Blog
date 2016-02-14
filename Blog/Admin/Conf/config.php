a49f3ce7c06df0800a83dd56d01ef3ded5cc9927
kongmile <943643887@qq.com>
2016-02-14 18:10:04 +0800

    修改数据库连接配置

--- Blog/Admin/Conf/config.php
+++ Blog/Admin/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'=>'mysql',//数据库类型
 	'DB_HOST'=>'localhost',//数据库服务器地址
-	'DB_NAME'=>'newsreport',//数据库名
+	'DB_NAME'=>'blog',//数据库名
 	'DB_USER'=>'root',//数据库用户
 	'DB_PWD'=>'',//数据库用户密码
 	'DB_PORT'=>'3306',//数据库端口(默认3306)，可以不配置
