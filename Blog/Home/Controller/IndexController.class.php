dc1f6c59c6fcfbf06b62ba488e1aa1b5452132c3
kongmile <943643887@qq.com>
2016-02-14 18:15:13 +0800

    显示文章列列表

--- Blog/Home/Controller/IndexController.class.php
+++ Blog/Home/Controller/IndexController.class.php
@@ -2,7 +2,22 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+
+	//显示首页
     public function index(){
-        echo "首页";
+		$articleDate = $this->showArticleList();//显示文章列表
+    }
+
+    //显示文章列表
+    private function showArticleList() {
+    	$article = M('article'); // 实例化artcle对象
+    	$p = I('get.id',1);//$p赋值
+		$list = $article->page($p.',5')->select();
+		$this->assign('list',$list);// 赋值数据集
+		$count      = $article->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数
+		$show       = $Page->show();// 分页显示输出
+		$this->assign('page',$show);// 赋值分页输出
+		$this->display(); // 输出模板
     }
 }
