eb346e729eaed2fefa19225be1689c9c1bee710b
kongmile <943643887@qq.com>
2016-02-15 19:21:06 +0800

    添加后台首首页

--- Blog/Admin/Controller/AdminController.class.php
+++ Blog/Admin/Controller/AdminController.class.php
@@ -3,6 +3,13 @@ namespace Admin\Controller;
 use Think\Controller;
 class AdminController extends Controller {
 
+    public function __construct() {
+        parent::__construct();
+        if((empty($_SESSION['auth'])) && (ACTION_NAME!=='login')){
+            $this->error('请先登录',U('Admin/login'),3);
+        }
+    }
+
     public function login(){
     	if($_POST){
     		$this->checklogin();
@@ -14,23 +21,12 @@ class AdminController extends Controller {
     public function checklogin(){
     	$authobj = D('Auth');
     	if($authobj->loginsubmit()){
-    		echo "<script> alert('yes'); </script>";
+    		$this->success('登陆成功','index',1);
     	}else{
-    		echo "<script> alert('no'); </script>";
+    		$this->error('账号或密码错误');
     	}
     }
 
-    public function __construct(){
-        parent::__construct();
-        dump(ACTION_NAME);
-        dump((ACTION_NAME!=='login'));
-    	if((empty($_SESSION['auth'])) && (ACTION_NAME!=='login')){
-    		echo "<script> alert('请先登录'); </script>";
-    	}else{
-    		echo "<script> alert('welcome'); </script>";
-
-    	}
-    }
 
     function hello(){
     	echo 'haha';
@@ -38,5 +34,22 @@ class AdminController extends Controller {
 
     function logout(){
         $_SESSION['auth'] = null;
+        $this->success('跳转中...','login',1);
     }
+
+    function edit(){
+        $this->display();
+    }
+
+    function index(){
+        $this->display();
+    }
+    function postNew(){
+        $test = M('article');
+        $data['content'] = $_POST['content'];
+        $data['title'] = $_POST['title'];
+        $data['summary'] = $_POST['summary'];
+        $test->add($data);
+    }
+    
 }
